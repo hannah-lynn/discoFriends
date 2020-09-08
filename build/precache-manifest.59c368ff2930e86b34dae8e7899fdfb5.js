f3bd1cd50a9fba9abb558201ab850d3ac5b71de8
Hannahvic <hannahvl1987@hotmail.com>
2020-09-08 17:23:43 +0100

    updating files

---
+++ build/precache-manifest.59c368ff2930e86b34dae8e7899fdfb5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03316c66c3ea159a56d83d25eab8bbe9",
+    "url": "/discoFriends/index.html"
+  },
+  {
+    "revision": "74656637e9ddf139bd72",
+    "url": "/discoFriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "bb31a5e0cb3dd3f0c094",
+    "url": "/discoFriends/static/css/main.b259951d.chunk.css"
+  },
+  {
+    "revision": "74656637e9ddf139bd72",
+    "url": "/discoFriends/static/js/2.115d98b8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/discoFriends/static/js/2.115d98b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb31a5e0cb3dd3f0c094",
+    "url": "/discoFriends/static/js/main.f4f7a2ea.chunk.js"
+  },
+  {
+    "revision": "7acb5ec716b73a5241bc",
+    "url": "/discoFriends/static/js/runtime-main.9f65707c.js"
+  },
+  {
+    "revision": "ffbb5d93ac5df62b35270dc94bf035f6",
+    "url": "/discoFriends/static/media/DiscoDiva.ffbb5d93.woff"
+  }
+]);
